5c19c2d245f42cc59ba4e9defd1d542b43e69813
soonh1 <johnny_tran_3@hotmail.com>
2020-11-28 15:08:34 +0100

    Blog

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MakersOfDenmarkModels.Lib;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakersOfDenmark.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: BlogController
+        public ActionResult Index()
+        {
+            //opret en web client
+            WebClient wc = new WebClient();
+            //hent data fra API (Kommer som json)
+            var json = wc.DownloadString("https://localhost:44399/api/blog");
+            //deserialisere det rå data til List<Student>
+            var options = new JsonSerializerOptions();
+            options.PropertyNameCaseInsensitive = true;
+            var result = JsonSerializer.Deserialize<List<Blog>>(json, options);
+            //Returnere
+            return View(result);
+        }
+
+        // GET: BlogController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BlogController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BlogController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BlogController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BlogController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BlogController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BlogController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
