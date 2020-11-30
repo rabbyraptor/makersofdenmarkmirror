e91b5f79fb024830fce199ef863c03fb12dd9377
Mathias Hammer <rabbyraptor@hotmail.com>
2020-11-30 14:09:37 +0100

    Update AktiviteterController.cs

--- Controllers/AktiviteterController.cs
+++ Controllers/AktiviteterController.cs
@@ -11,13 +11,14 @@ namespace MakersOfDenmark.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var aktivitet = new Aktivitet { Id = "1", Title = "Norwich Science Festival", Date = "27. november", StartTime = "kl. 16:00", MakerSpace = "FabLab UCL", Address = "Seebladsgade 1, 5000 Odense", ImgUrl = "/img/aktiviteter/1.jpg" };
+            return View(aktivitet);
         }
 
         // 
         // GET: /Aktiviteter/id/ 
 
-        public IActionResult Id(int? id)
+        public IActionResult Id(int id)
         {
             var aktivitet = new Aktivitet { Id = id.ToString(), Title = "Norwich Science Festival", Date = "27. november", StartTime = "kl. 16:00", MakerSpace = "FabLab UCL", Address = "Seebladsgade 1, 5000 Odense", ImgUrl = "/img/aktiviteter/1.jpg" };
             return View(aktivitet);
