793769d53ceae30057c52538f4fea03f11b14e23
Mathias Hammer <rabbyraptor@hotmail.com>
2020-12-01 12:37:26 +0100

    Update MakerspaceModel.cs

--- Models/MakerspaceModel.cs
+++ Models/MakerspaceModel.cs
@@ -5,5 +5,9 @@ namespace MakersOfDenmark.Models
     public class Makerspace
     {
         public string Id { get; set; }
+        public string Title { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string HeaderImage { get; set; }
     }
 }
