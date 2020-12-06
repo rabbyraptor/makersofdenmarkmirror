b8ddc9b1510c2ed4cc7ccbbcf86cdf749cdf4713
Mathias Hammer <rabbyraptor@hotmail.com>
2020-12-06 13:38:22 +0100

    Added api GET connections

--- Controllers/MakerspaceController.cs
+++ Controllers/MakerspaceController.cs
@@ -1,27 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 using MakersOfDenmark.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace MakersOfDenmark.Controllers
 {
-    public class Makerspace : Controller
+    public class MakerspaceController : Controller
     {
+        HttpClientHandler _clientHandler = new HttpClientHandler();
+        Makerspace _oMakerspace = new Makerspace();
+        List<Makerspace> _oMakerspaces = new List<Makerspace>();
+
+        public MakerspaceController()
+        {
+            _clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+        }
+
+        //
+        // GET ALL MAKERSPACES FROM API
+        [HttpGet]
+        public async Task<List<Makerspace>> GetAllMakerspaces()
+        {
+            _oMakerspaces = new List<Makerspace>();
+
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var response = await httpClient.GetAsync("https://fablabsandbox.azurewebsites.net/api/Makerspaces"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    _oMakerspaces = JsonConvert.DeserializeObject<List<Makerspace>>(apiResponse);
+                }
+            }
+
+            for (int i = 0; i < _oMakerspaces.Count; i++)
+            {
+                //_oMakerspaces[i].makerSpaceName = _oMakerspaces[i].makerSpaceName.ToLower();
+            }
+
+            return _oMakerspaces;
+        }
+
+        //
+        // GET SINGLE MAKERSPACE FROM API
+        // (VIRKER IKKE UDEN AUTH VIA JWT-TOKEN)
+        /* [HttpGet]
+        public async Task<Makerspace> GetById(string makerSpaceId)
+        {
+            _oMakerspace = new Makerspace();
+
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var response = await httpClient.GetAsync("https://fablabsandbox.azurewebsites.net/api/Makerspaces" + makerSpaceId))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    _oMakerspace = JsonConvert.DeserializeObject<Makerspace>(apiResponse);
+                }
+            }
+
+            return _oMakerspace;
+        } */
+
         // 
         // GET: /Makerspace/
-
         public IActionResult Index()
         {
-            var makerspace = new Models.Makerspace { Id = "1" };
-            return View(makerspace);
+            try
+            {
+                var response = GetAllMakerspaces().Result;
+                return View(response);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return View();
+            }
         }
 
         // 
-        // GET: /Makerspace/Id/ 
-
+        // GET: /Makerspace/Name/ 
         public IActionResult Id(string id)
         {
-            var makerspace = new Models.Makerspace { Id = id, Title = "FabLab UCL", Address = "Seebladsgade 1, 5000 Odense C", Email = "fablab@ucl.dk", HeaderImage = "/img/makerspace/makerspace-header.jpg" };
-            return View(makerspace);
+            try
+            {
+                var response = GetAllMakerspaces().Result;
+                var makerspaceIndex = response.FindIndex(x => x.makerSpaceName == id);
+                return View(response[makerspaceIndex]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return View();
+            }
         }
     }
 }
